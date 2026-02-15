f48a0201426cbc5ef7e25f12f5eb191d08c028b5
Suphatith01 <suphachitkoprasroetsud@gmail.com>
2026-02-15 11:08:40 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import Header from './section/Header'
+import Navbar from './section/Navbar'
+import Contact from './section/Contact'
+import LeftSection from './section/LeftSection'
+import RightSection from './section/RightSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='mt-14 mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
-      <div>
-        <div className='sticky top-14 grid grid-rows-[35%_40%_25%] h-screen'>
-          <div className='flex flex-col gap-2'>
-            <div className='text-3xl text-primaryTitle'>Suphatith Koprasroetsud</div>
-            <div className='text-primaryAccent font-semibold'>Software Developer · between jobs</div>
-            <div className='text-sm w-5/6'>From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.</div>
-            <div className= 'mt-4'>
-              <span className='rounded-md bg-primaryTitle text-white py-2 px-4 '>View Resume</span>
-            </div>
-          </div>
-          <div>Nav</div>
-          <div className='flex items-end'>Link</div>
-        </div>
-      </div>
-      <div>
-        <div >About</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-      </div>
+      <LeftSection />
+      <RightSection />
     </div>
   )
 }
