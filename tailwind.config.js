ad2c519d8782e38a9bd21aee6111ae38dbd66d56
Suphatith01 <suphachitkoprasroetsud@gmail.com>
2026-02-15 09:49:52 +0700

    update app

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ export default {
   theme: {
     extend: {
       colors:{
-       primaryTitle: '#012340',
-       primaryContent: '#025959',
-       primarySubcontent: '#027333',
-       primaryBase: '#03A63C',
-       primaryAccent: '#04D939',
-       primaryBg: '#f6f6f6',
+        primaryTitle: '#127369',
+        primaryContent: '#4C5958',
+        primarySubcontent: '#8AA6A3',
+        primaryBase: '#a9b5b4',
+        primaryAccent: '#10403B',
+        primaryBg: '#f6f6f6',
       }
     },
   },
