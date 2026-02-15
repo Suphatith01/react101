3af91b27aee577a49e5d87a48a392b1e2b0badae
Suphatith01 <suphachitkoprasroetsud@gmail.com>
2026-02-15 11:31:00 +0700

    add contents header.js

---
+++ src/contents/header.js
@@ -0,0 +1,8 @@
+import fileName from '../assets/cv.pdf'
+export const data = {
+    "name" : "Suphatith Koprasroetsud",
+    "title" : "Software Developer · between jobs",
+    "caption" : "From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
+    btnText: "View Resume",
+    link: fileName
+}
